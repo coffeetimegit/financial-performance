b45e627f49591a0a5cf2ef6d3081b8aacfd30004
Yutaka Obi <obiyutak@gmail.com>
2022-07-06 17:44:44 +0900

    make it better

--- company_profile.py
+++ company_profile.py
@@ -96,7 +96,7 @@ def page_count(url_list_xpath):
 
 def get_reports(df, company_list, pages):
     for i in range(pages):
-        url = get_url(i * 10 0)
+        url = get_url(i * 100)
         driver.get(url)
         df, company_list = filter_irrelevant_rows(df, table_row_xpath,
                                                   company_list, f"{table_row_xpath}{company_xpath}",
